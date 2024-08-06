c7de66f834036e8f2247aea69539aa8f1ad8e360
Bezalel <bs0583251661@gmail.com>
2024-08-06 11:38:11 +0300

    EX 1,2,3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AtteckServer
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+            //await GetDataAsync();
+            ////Console.WriteLine(await GetDataAsync());
+            //Console.WriteLine(await ReadFileAsinc(@"C:\Users\internet\source\repos\AtteckServer\TextFile1.txt"));
+            //Console.WriteLine("recived data");
+
+            Task<string> TaskA = GetDataFromServiceAAsinc();
+            Task<string> TaskB = GetDataFromServiceBAsinc();
+
+            await Task.WhenAll(TaskA, TaskB);
+
+            Console.WriteLine(TaskA.Result);
+            Console.WriteLine(TaskB.Result);
+            Console.ReadLine();
+
+        }
+
+        public static async Task<string> GetDataAsync()
+        {
+            await Task.Delay(2000);
+            return "daownloed";
+        }
+
+        public static async Task<string> ReadFileAsinc(string path) 
+        {
+            string contents = await Task.Run(() => File.ReadAllText(path));
+            
+            return contents;
+        }
+
+        public static async Task<string> GetDataFromServiceAAsinc()
+        {
+            await Task.Delay(1000);
+            return "data from service A";
+        }
+
+        public static async Task<string> GetDataFromServiceBAsinc()
+        {
+            await Task.Delay(2000);
+            return "data from service B";
+
         }
     }
 }
